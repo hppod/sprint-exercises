f4dc4f18fd1c9471b81984818de60db672f7e791
hppod <hppodgornik@gmail.com>
2019-01-24 21:28:49 -0200

    01

---
+++ 01/calculator/test/calculator-test.js
@@ -0,0 +1,9 @@
+/**
+ * Arquivo: calculator.js
+ * Author: Hygor Podgornik
+ * Description: Arquivo que contém os testes dos métodos das 4 operações básicas de uma calculadora
+ * Date: 24/01/2019
+ */
+
+const chai = require('chai');
+const calculator = require('../app/calculator')
