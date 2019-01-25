09142888da473426603cfa1defcff165e18da2bd
hppod <hppodgornik@gmail.com>
2019-01-25 21:01:51 -0200

    03

---
+++ 03/http-tests/test/voo-test.js
@@ -0,0 +1,10 @@
+/**
+ * File: test/voo-test.js
+ * Author: Hygor Podgornik
+ * Description: Arquivo responsável por realizar os testes com Chai e Mocha
+ * Date: 25/01/2019
+ */
+
+ /**
+ * ESSE ARQUIVO DEVE CONTER 5 (CINCO) MÉTODOS DE TESTE CONFORME OS METODOS DA API DO ARQUIVO "voo.js"
+ */
